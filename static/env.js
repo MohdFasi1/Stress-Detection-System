74e5787bbef3a03bd0a62af95b8311b17af2afd5
MohdFasi1 <mohdfasi7451@gmail.com>
2025-06-16 22:59:59 +0530

    firebase integration

---
+++ static/env.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCNSajXUTTladTiGT7_Njzz9CjUmN98wOM",
+  authDomain: "stress-detection-1e92a.firebaseapp.com",
+  projectId: "stress-detection-1e92a",
+  storageBucket: "stress-detection-1e92a.firebasestorage.app",
+  messagingSenderId: "643808453504",
+  appId: "1:643808453504:web:b6b8966e74a3bf4a595765",
+  measurementId: "G-816SBDJVSN"
+};
+        firebase.initializeApp(firebaseConfig);
