dbd45836f66f209f80bb95e132674ab448bcdab9
MohdFasi1 <mohdfasi7451@gmail.com>
2025-06-16 23:03:58 +0530

    firebase integration

---
+++ static/env.example.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "Your-ApiKey",
+  authDomain: "Your-authDomain",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+        firebase.initializeApp(firebaseConfig);
